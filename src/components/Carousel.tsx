a97d3219c99c91db688e3ffdeb8399196fde15ca
Betelhem Yemane <betelhemyemanezoom@gmail.com>
2025-06-17 12:55:34 -0700

    small tweak

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,14 +2,13 @@
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 // import Image from 'next/image';
 
 const Swiper = dynamic(() => import('swiper/react').then((mod) => mod.Swiper), { ssr: false });
 const SwiperSlide = dynamic(() => import('swiper/react').then((mod) => mod.SwiperSlide), { ssr: false });
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-
 
 
 type CarouselImage = {
